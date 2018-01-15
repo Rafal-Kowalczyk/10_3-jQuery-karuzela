24bfaba99943a914641859d84781f70e78ff9694
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-01-15 15:32:16 +0100

    Fix scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function(){
+	
+	var carouselList = $("#carousel ul");
+
+	setInterval(changeSlide, 3000);
+
+	function changeSlide(){
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		}
+	
+
+	function moveFirstSlide(){
+		carouselList
+			.find("li:last")
+			.after(carouselList.find("li:first"));
+		carouselList.css({marginLeft: 0});
+	};
+});
